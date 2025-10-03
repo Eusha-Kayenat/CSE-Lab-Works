d53f535c236544f57746d170a0607a07d373a4ab
Eusha Kayenat <eushakayenat@gmail.com>
2025-10-03 21:07:44 +0600

    Create Task 3.java

---
+++ CSE 221/Lab 1/Task 3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); 
+        
+        for (int i = 0; i < T; i++) {
+            long N = sc.nextLong(); 
+            long sum = N * (N + 1) / 2;
+            System.out.println(sum);
+        }
+    }
+}
