7126f6366c3cd02a5530fb1865799a6e0488b8df
Eusha Kayenat <eushakayenat@gmail.com>
2025-10-03 21:41:41 +0600

    Create G.py

---
+++ CSE 221/Lab 4/G.py
@@ -0,0 +1,18 @@
+N, M, K = map(int, input().split())
+ 
+knights = set()
+for _ in range(K):
+    x, y = map(int, input().split())
+    knights.add((x, y))
+ 
+moves = [(-2, -1), (-2, 1), (-1, -2), (-1, 2),
+         (1, -2), (1, 2), (2, -1), (2, 1)]
+ 
+for x, y in knights:
+    for dx, dy in moves:
+        nx, ny = x + dx, y + dy
+        if (nx, ny) in knights:
+            print("YES")
+            exit()
+ 
+print("NO")
