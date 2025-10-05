feda344c39fb7c3f799d7b15cffdf2266908f1b3
Eusha Kayenat <eushakayenat@gmail.com>
2025-10-05 21:47:58 +0600

    Create Task 2.py

---
+++ CSE 111/Lab 4/Classwork/Task 2.py
@@ -0,0 +1,24 @@
+class Panda:
+  def __init__(self,x,y,z):
+    self.name=x
+    self.gender=y
+    self.age=z
+  def sleep(self,s):
+    if 3<=s<=5:
+      return f"{self.name} sleeps {s} hours daily and should have Mixed Veggies"
+    elif 6<=s<=8:
+      return f"{self.name} sleeps {s} hours daily and should have Eggplant & Tofu"
+    elif 9<=s<=11:
+      return f"{self.name} sleeps {s} hours daily and should have Broccoli Chicken"
+    else:
+      return f"{self.name}'s duration is {s} thus should have only bamboo leaves"
+panda1=Panda("Kunfu","Male",5)
+panda2=Panda("Pan Pan","Female",3)
+panda3=Panda("Ming Ming","Female",8)
+print("{} is a {} Panda Bear who is {} years old".format(panda1.name,panda1.gender,panda1.age))
+print("{} is a {} Panda Bear who is {} years old".format(panda2.name,panda2.gender,panda2.age))
+print("{} is a {} Panda Bear who is {} years old".format(panda3.name,panda3.gender,panda3.age))
+print("===========================")
+print(panda2.sleep(10))
+print(panda1.sleep(4))
+print(panda3.sleep(13))
