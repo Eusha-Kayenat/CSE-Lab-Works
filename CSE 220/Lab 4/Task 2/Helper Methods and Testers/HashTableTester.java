91ca82cf6c1479e545fe39e4b76424ea6785550c
Eusha Kayenat <eushakayenat@gmail.com>
2025-10-06 22:21:31 +0600

    Add files via upload

--- CSE 220/Lab 4/Task 2/Helper Methods and Testers/HashTableTester.java
+++ CSE 220/Lab 4/Task 2/Helper Methods and Testers/HashTableTester.java
@@ -2,74 +2,31 @@
 public class HashTableTester {
     //DO NOT TOUCH THIS TESTER MAIN METHOD    
     public static void main(String[] args){
-
-        //DO NOT TOUCH ANY CODE BELOW
-        Object[][] pairs = {
-            {"Colt",360},
-            {"Cordelius",730},
-            {"Shelly",300},
-            {"Doug",1200},
-            {"Emz",520},
-            {"Bo",580}
-        };
-
-        int size = pairs.length;
-        HashTable ht = new HashTable( size );
-        ht.createFromArray(pairs);
-        ht.printHashTable();
-
-        Object[] pair = new Object[2];
-        pair = new Object[]{"Doug",1200};
-        String result = ht.searchHashtable( pair );
-        System.out.println("----------------------");
-        System.out.println("Searching (Doug,1200) ");
-        System.out.println("----------------------");
-        System.out.println("Excepted Output: Found");
-        System.out.println("Your Output    : "+result);
-        System.out.println("----------------------");
-        System.out.println("Inserting (Doug,1200) ");
-        System.out.println("----------------------");
-        System.out.println("Expected Output: (Doug,1200) already Inserted. Cannot reinsert.");
-        System.out.print(  "Your Output    : ");
-        ht.insert( pair );
-        System.out.println("============================================\n");
         
-        pair = new Object[]{"Edgar",320};
-        result = ht.searchHashtable( pair );
-        System.out.println("----------------------");
-        System.out.println("Searching (Edgar,320) :");
-        System.out.println("----------------------");
-        System.out.println("Excepted Output: Not Found");
-        System.out.println("Your Output    : "+result);
-        System.out.println("----------------------");
-        System.out.println("Inserting (Edgar,320) ");
-        System.out.println("----------------------");
-        String exOut = "::::Excepted Output::::\n"
-                    + "0 : (Key: Doug, Value: 1200) --> null\n"
-                    + "1 : (Key: Shelly, Value: 300) --> null\n"
-                    + "2 : (Key: Cordelius, Value: 730) --> null\n"
-                    + "3 : (Key: Edgar, Value: 320) --> (Key: Bo, Value: 580) --> (Key: Emz, Value: 520) --> (Key: Colt, Value: 360) --> null\n"
-                    + "4 : null"
-                    + "5 : null";
-        System.out.println( exOut );
-        System.out.println("::::Your Output::::");
-        ht.insert( pair );
-        ht.printHashTable();
-        System.out.println("============================================\n");
+        //DO NOT TOUCH ANY CODE BELOW
+        HashTable ht = new HashTable( 3 );
 
-        pair = new Object[]{"Edgar",320};
-        result = ht.searchHashtable( pair );
-        System.out.println("----------------------");
-        System.out.println("Searching (Edgar,320) ");
-        System.out.println("----------------------");
-        System.out.println("Excepted Output: Found");
-        System.out.println("Your Output    : "+result);
-        System.out.println("----------------------");
-        System.out.println("Inserting (Edgar,320) ");
-        System.out.println("----------------------");
-        System.out.println("Excepted Output: (Edgar,320) already Inserted. Cannot reinsert.");
-        System.out.print("Your Output    : ");
-        ht.insert( pair );
-        System.out.println("============================================\n");
+        System.out.println("====================Test#1====================");
+        ht.insert("apple", 20);
+        ht.insert("coconut", 90);
+        ht.insert("cherry", 50);
+        System.out.println(":::Expected Output:::\n0 ('coconut', 90) --> \n1 ('apple', 20) --> \n2 ('cherry', 50) --> ");
+        System.out.println("---------------------");
+        System.out.println(":::  Your Output  :::");
+        ht.show();
+        System.out.println("====================Test#2====================");
+        ht.insert("banana", 30);
+        ht.insert("pineapple", 50);
+        System.out.println(":::Expected Output:::\n0 ('coconut', 90) --> ('pineapple', 50) --> ('banana', 30) -->\n1 ('apple', 20) -->\n2 ('cherry', 50) -->");
+        System.out.println("---------------------");
+        System.out.println(":::  Your Output  :::");
+        ht.show();
+        System.out.println("====================Test#3====================");
+        ht.insert("apple", 100);
+        ht.insert("guava", 10);
+        System.out.println(":::Expected Output:::\n0 ('coconut', 90) --> ('pineapple', 50) --> ('banana', 30) -->\n1 ('apple', 100) --> ('guava', 10) -->\n2 ('cherry', 50) -->");
+        System.out.println("---------------------");
+        System.out.println(":::  Your Output  :::");
+        ht.show();
     }
 }
