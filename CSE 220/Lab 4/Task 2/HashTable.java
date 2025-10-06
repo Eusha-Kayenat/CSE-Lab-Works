08651d920a79c09c91d5a41f84d8900f36067af5
Eusha Kayenat <eushakayenat@gmail.com>
2025-10-06 22:24:27 +0600

    Update HashTable.java

--- CSE 220/Lab 4/Task 2/HashTable.java
+++ CSE 220/Lab 4/Task 2/HashTable.java
@@ -1,21 +1,8 @@
-/*You will have to complete the FruitNode Contrustor first
- then within this class you only have to complete two methods
- hashFunction() and insert()
- the rest of the metods are already written
- DO NOT TOUCH any other methods or codes*/
 public class HashTable {
-
-    //ht[] :: is the HashTable array that stores the FruitNode objects
     private FruitNode[] ht;
-
-    //Constructor that initializes the HashTable array
-	//DO NOT change this Constructor
     public HashTable(int size){
         this.ht = new FruitNode[size];
     }
-    
-    //This method basically prints the HashTable
-    //DO NOT change this method
     public void show(){
         for(int i=0; i<ht.length; i++){
             System.out.print( i+" " );
@@ -27,19 +14,51 @@ public class HashTable {
             System.out.println();
         }
     }
-
-    //you need to COMPLETE this method
     private int hashFunction( String key ){
-        // TO DO 
-        return -1; //remove this line
+      int sum = 0;
+      if(key.length()%2==0){
+        for(int i=0;i<key.length();i=i+2){
+          sum+=(int)key.charAt(i);
+        }
+      }
+      else{
+        for(int i=1;i<key.length();i=i+2){
+          sum+=(int)key.charAt(i);
+        }
+      }
+        int val = sum%ht.length;
+        return val;
     }
-
-    //you need to COMPLETE this method
-    //The insert() method will create a FruitNode using name(Key) & price(value)
-	//then inserts it in the proper hashed index
-    //If collision occurs resolve using the steps explained in the question
     public void insert(String key, Integer value){
-        // TO DO
+        FruitNode newNode = new FruitNode(key,value);
+        int index = hashFunction(key);
+        if(ht[index]==null){
+          ht[index]=newNode;
+        }
+        else{
+           FruitNode current = ht[index];
+            while(current!=null){
+              if(current.fruit[0].equals(key)){
+               current.fruit[1]=value;
+               return;
+              }
+              current=current.next;
+            }
+           current = ht[index];
+           FruitNode prev = null;
+           while(current!=null && (int)current.fruit[1]>(int)newNode.fruit[1]){
+             prev = current;
+             current = current.next;
+           }
+           if(prev==null){
+           newNode.next=current;
+           ht[index]=newNode;
+           }
+           else{
+              prev.next=newNode;
+             newNode.next=current;       
+           }
+        }
     }
 
 }
