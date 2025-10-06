083d628b9eb8d33118be8e47443c585517995f16
Eusha Kayenat <eushakayenat@gmail.com>
2025-10-06 21:37:52 +0600

    Update Task 1.java

--- CSE 220/Lab 2/Task 1.java
+++ CSE 220/Lab 2/Task 1.java
@@ -1,1 +1,26 @@
-
+public class Task1{
+  public static Node alternateMerge( Node head1, Node head2 ){
+      Node h1 = head1;
+      Node h2 = head2;
+      if (h1 == null){ 
+        return h2;
+      }
+      if (h2 == null){
+        return h1;
+      }
+      while (h1!=null && h2!=null){
+            Node temp1 = h1.next;
+            Node temp2 = h2.next;
+            h1.next=h2;
+            if (temp1 == null){ 
+              break;
+            }
+            else{
+            h2.next = temp1;
+            h1 = temp1;
+            h2 = temp2;
+           }
+          }
+ 
+         return head1;
+  }
